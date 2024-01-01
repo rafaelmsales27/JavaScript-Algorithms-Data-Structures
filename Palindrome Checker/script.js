57d4ebdb68e477ff1ad2d13d8dbc90401ab5f864
rafaelmsales27 <rafaelmsales27@gmail.com>
2024-01-01 12:07:43 +0100

    palindrome checker 2

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,10 +1,34 @@
 const checkBtn = document.querySelector('#check-btn');
-const textInput = document.querySelector('#text-input');
-const text = textInput.textContent;
-checkBtn.addEventListener("click", () => {
-    if (text === null || text === "")  {
-        alert('Please input a value');
-    } else if (text = "A") {
+const result = document.getElementById('result');
 
+checkBtn.addEventListener("click", () => {
+    const textInput = document.getElementById('text-input').value;
+
+    if (textInput === "" || textInput === null){
+        alert('Please input a value');
+    } else if (isPalindrome(textInput)){
+        result.textContent = `${textInput} is a palindrome`;
+    } else {
+        result.textContent = `${textInput} is not a palindrome`;
     }
-})
+});
+
+const isPalindrome = (word) => {
+    //Ignore non alphanumeric characters
+    const wordwithOnlyAlphanumeric = word.replace(/[^a-zA-Z0-9]/g, '');
+    console.log("only alpha: " + wordwithOnlyAlphanumeric);
+    //remove spaces
+    const wordWithoutSpaces = wordwithOnlyAlphanumeric.replace(/\s/g, '');
+    console.log("without space: " + wordWithoutSpaces);
+    //lowercase all chars
+    const wordLowerCase = wordWithoutSpaces.toLowerCase();
+    console.log("lower: " + wordLowerCase);
+    //reverse word
+    const reverseWord = wordLowerCase.split('').reverse().join('');    
+    console.log("reverse: " + reverseWord);
+    if (wordLowerCase === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
